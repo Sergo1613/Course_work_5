26ba4ee0c4e79c3e85139e378e706feb9af753f0
Sergo1613 <mentoss2012@gmail.com>
2023-07-21 15:27:52 +0300

    update utils.py

--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@ import requests
 
 
 def get_employers_data():
-    employers_id = [78638, 1740, 15478, 39305, 4771, 3982116, 4039, 3529, 1375441, 2180]
+    employers_id = [3529, 2324020, 5658700, 3183420, 8178, 3776, 906557, 4889905, 2000762, 10079206]
     all_employers = []
     for id in employers_id:
         url = f"https://api.hh.ru/employers/{id}"
@@ -25,7 +25,7 @@ def get_employers_data():
 def get_data_vacancies():
     vacancies_url = f"https://api.hh.ru/vacancies"
     params = {
-        "employer_id": [78638, 1740, 15478, 39305, 4771, 3982116, 4039, 3529, 1375441, 2180],
+        "employer_id": [3529, 2324020, 5658700, 3183420, 8178, 3776, 906557, 4889905, 2000762, 10079206],
         "page": 0,
         "per_page": 100
     }
